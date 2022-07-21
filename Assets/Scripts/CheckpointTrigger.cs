def962d7e7a59b2d15d38d1535080f1d50db9569
Freak Wadliwa <freakwadliwa@mail.ru>
2022-07-21 21:09:06 +0300

    deathScreen

--- Assets/Scripts/CheckpointTrigger.cs
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -6,16 +6,30 @@ namespace Assets.Scripts
     {
         [SerializeField]
         private Checkpoint _checkpoint;
-        
+        [SerializeField]
+        private GameObject _deathScreen;
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player"))
             {
                 return;
             }
+            _deathScreen.SetActive(true);
             other.transform.position = _checkpoint.transform.position;
             other.transform.rotation = Quaternion.identity;
+            Time.timeScale = 0f;
         }
 
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1f;
+                _deathScreen.SetActive(false);
+
+            }
+        }
+     
     }
 }
